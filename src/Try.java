d195bee49455e694bdec137a5b06984cfd2ab543
MonishaTerzo <monisha@terzocloud.com>
2023-06-16 14:15:19 +0530

    sample

--- src/Try.java
+++ src/Try.java
@@ -1,17 +1,11 @@
 public class Try {
-
     public static void main(String[] args) {
         show();
     }
-        public  static void show(){
-        greeting(new Interface() {
-            @Override
-            public void main(int l) {
-                System.out.println("hi..........");
-            }
-        }) ; }
+    public  static void show(){
+        greeting(
+            m-> System.out.println("monisha")); }
         public  static void greeting(Interface simple){
-            simple.main(60);
-    }
+            simple.main(60);}
 
 }
--- src/imple.java
+++ src/imple.java
@@ -6,4 +6,5 @@ public class imple implements Interface{
 
 
 
+
 }
